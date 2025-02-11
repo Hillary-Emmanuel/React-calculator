7604f53c640b0ac5c1c35018f72f0f9a93e0d0f0
Hillary Emmanuel <hillaryemmanuel80@gmail.com>
2025-02-11 03:31:49 +0100

    update result of my Reactjs calculator

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function formatNumber(value) {
+  // Convert the number to a string and format with underscores
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '_');
+}
+
 function Display({ value }) {
   return (
     <div className='display'>
-      <input type='text' value={value} readOnly />
+      <input type='text' value={formatNumber(value)} readOnly />
     </div>
   );
 }
